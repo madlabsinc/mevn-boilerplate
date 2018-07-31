8ad90cee43c002bf6e0090e61261b6cef0ced7d9
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-31 22:24:04 +0530

    fix lint errors

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Importing required modules
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import winston from './config/winston'
+
+// Defining port
+const port = process.env.PORT || 9000
+
+// Defining app
+const app = express()
+
+// Defining middlewares
+app.use(morgan('combined', { stream: winston.stream }))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Defining the Routes
+require('./routes/index')(app)
+
+// Listening to port
+app.listen(port)
+console.log('Authentication Service Running On:' + ' ' + 'http://localhost:' + port)
+
+module.exports = app
