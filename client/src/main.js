b975ff399f2228357d45de73d7794f3480d9c32d
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-10-24 21:09:53 +0530

    Temporary workaround :+1:

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
